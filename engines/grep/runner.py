76209d52d9a3fcc0f911659e7e442476009d5b88
Grayson Nocera <gnocera@purdue.edu>
2024-04-11 12:06:48 -0400

    get time to run for grep and hyperscan

--- engines/grep/runner.py
+++ engines/grep/runner.py
@@ -2,15 +2,14 @@ import subprocess
 import os
 import re
 
-# Get these from the CLI program via stdin
+# TODO: et these from the CLI program via stdin
 haystack = "xxxxx=xxxxxx"
 pattern = r'x=x'
 
 temp_file = os.path.join(os.path.dirname(__file__), "temp")
 
-if not os.path.exists(temp_file):
-  with open(temp_file, "w") as f:
-      f.write(haystack)
+with open(temp_file, "w") as f:
+    f.write(haystack)
 
 command = "time grep " + pattern + " " + temp_file
 result = subprocess.run(command, capture_output=True, shell=True, cwd=os.path.dirname(__file__), text=True)
@@ -22,7 +21,7 @@ for line in result.stderr.splitlines():
   if match:
     output[match.group(1)] = float(match.group(2)) * 60 + float(match.group(3))
 
-print(output['user'])
+print(output)
 
 os.remove(temp_file)
   
--- engines/hyperscan/runner.py
+++ engines/hyperscan/runner.py
@@ -2,30 +2,39 @@ import hyperscan as hs
 from typing import Any, Optional
 import timeit
 
+haystack = "xxxxx=xxxxxx"
+pattern = r'x=x'
+
 # A truthy return value signifies to stop matching
 def on_match(
     id: int, from_: int, to: int, flags: int, context: Optional[Any] = None
 ) -> Optional[bool]:
-    print(f"Matched pattern {id} from {from_} to {to}")
-    print(f"Flags: {flags}")
-    print(f"Context: {context}")
-
+    pass
 
 # Caveat: this uses Vectorscan rather than Hyperscan, but the two are similar
+# should the CLI provide us with BLOCK, STREAM, or VECTOR mode?
+
+def setup_to_timeit(pattern: str):
+    import hyperscan as hs
+    db = hs.Database(mode=hs.HS_MODE_BLOCK)
+    patterns = (
+        (pattern.encode(), 0),
+    )
+    expressions, ids = zip(*patterns)
+    return db, expressions, ids
+
+# compile the pattern
 db = hs.Database(mode=hs.HS_MODE_BLOCK)
 patterns = (
-    (rb"foo", 0),
-    (rb"bar", 1),
+    (pattern.encode(), 0),
 )
-
 expressions, ids = zip(*patterns)
-db.compile(expressions=expressions, ids=ids, elements=len(patterns))
-print(db.info().decode())
+time_to_compile = timeit.timeit(stmt=lambda: db.compile(expressions=expressions, ids=ids, elements=len(patterns)), number=1)
+print(f"time to compile: {time_to_compile}")
 
 # BLOCK MODE
-print(db.scan(b"foo", match_event_handler=on_match, context="foo"))
-
-print()
+time_to_search = timeit.timeit(stmt=lambda: db.scan(haystack.encode(), match_event_handler=on_match), number=1)
+print(f"time to search: {time_to_search}")
 
 # STREAM MODE
 db = hs.Database(mode=hs.HS_MODE_STREAM)
