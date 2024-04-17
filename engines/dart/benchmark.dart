a48164026853c7c527c77888e049ac8d8723daf0
Grayson Nocera <gnocera@purdue.edu>
2024-04-17 13:24:30 -0400

    can't run dart2native

--- engines/dart/benchmark.dart
+++ engines/dart/benchmark.dart
@@ -1,26 +1,17 @@
 import 'dart:io';
 
 main(List<String> arguments) {
-  if (arguments.length <= 1) {
-    print('Usage: dart bencharmark.dart <filename> regex1 regex2 ... regexN');
+  if (arguments.length != 3) {
+    print('Usage: dart bencharmark.dart <filename> regex numIterations');
     exit(1);
   }
 
   new File(arguments[0])
     .readAsString()
     .then((String data) {
-      
       for (var i = 1; i < arguments.length; i++) {
         measure(data, arguments[i]);
       }
-      // // Email
-      // measure(data, r"[\w.+-]+@[\w.-]+\.[\w.-]+");
-
-      // // URI
-      // measure(data, r"[\w]+:\/\/[^\/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?");
-
-      // // IP
-      // measure(data, r"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])");
     });
 }
 
