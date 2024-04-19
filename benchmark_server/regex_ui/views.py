af69bdfaaaf872cba09acb73dd351f554b8d1596
Parth Patil <parthvin@gmail.com>
2024-04-19 18:51:35 -0400

    added django server

---
+++ benchmark_server/regex_ui/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def landing_page(request):
+    # return HttpResponse('Hello World')
+    return render(request, 'landing.html')
