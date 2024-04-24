08a9df823d08fb9708a8106a1accf0d9e2341a64
Grayson Nocera <gnocera@purdue.edu>
2024-04-24 15:51:01 -0400

    add error handling for dart

--- engines/dart/benchmark.dart
+++ engines/dart/benchmark.dart
@@ -1,28 +1,37 @@
 import 'dart:io';
 
+const EXIT_FAILURE = 1;
+
 main(List<String> arguments) {
   if (arguments.length != 3) {
     print('Usage: dart bencharmark.dart <filename> regex numIterations');
     exit(1);
   }
 
+  var numIterations = int.parse(arguments[2]);
+  var pattern = arguments[1];
   new File(arguments[0])
     .readAsString()
     .then((String data) {
-      for (var i = 1; i < arguments.length; i++) {
-        measure(data, arguments[i]);
+      for (var i = 0; i < numIterations; i++) {
+        measure(data, pattern);
       }
     });
 }
 
 measure(data, pattern){
-  RegExp exp = new RegExp(pattern);
+  try {
+    RegExp exp = new RegExp(pattern);
 
-  var stopwatch = new Stopwatch()..start();
-  Iterable<Match> matches = exp.allMatches(data);
-  var count = matches.length;
+    var stopwatch = new Stopwatch()..start();
+    Iterable<Match> matches = exp.allMatches(data);
+    var count = matches.length;
 
-  stopwatch.stop();
+    stopwatch.stop();
 
-  print('${stopwatch.elapsedMicroseconds / 1e3} - ${count}');
+    print('${stopwatch.elapsedMicroseconds / 1e3} - ${count}');
+  } on Exception catch (e) {
+    stderr.write('compilation failed: $e\n');
+    exit(EXIT_FAILURE);
+  }
 }
