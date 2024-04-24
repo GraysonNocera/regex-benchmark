960faa809ea5aa7bb1a7a6f40367cb1e3f7a2a20
Grayson Nocera <gnocera@purdue.edu>
2024-04-24 11:42:19 -0400

    updates to PCRE2

--- engines/c/benchmark.c
+++ engines/c/benchmark.c
@@ -2,12 +2,26 @@
 #include <string.h>
 #include <sys/time.h>
 
+// This errors for something like [\w-\.] because of ranges of character classes
+// the "-" must be escaped
+
 #if __has_include(<time.h>)
 #include <time.h>
 #endif
 
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
+
+#define OK_COMPILE_CODE 100
+
+void print_error(int errorcode, int erroroffset) {
+  int error_length = 5000; // just a guess
+  char * error_buffer = malloc(sizeof(char) * error_length);
+  int retval =  pcre2_get_error_message(errorcode, (PCRE2_UCHAR *) error_buffer, (PCRE2_SIZE) error_length);
+  fprintf(stderr, "Error at position %d: %s\n", erroroffset, error_buffer);
+  free(error_buffer);
+  return;
+}
 
 char *read_file(char *filename)
 {
@@ -33,27 +47,42 @@ char *read_file(char *filename)
   return data;
 }
 
-void measure(char *data, char *pattern)
+int measure(char *data, char *pattern)
 {
   int count = 0;
   double elapsed;
   struct timespec start, end;
   pcre2_code *re;
+
   int errorcode;
   PCRE2_SIZE erroroffset;
+
   pcre2_match_data *match_data;
-  int length;
   PCRE2_SIZE offset = 0;
   PCRE2_SIZE *ovector;
 
-  re = pcre2_compile((PCRE2_SPTR)pattern, PCRE2_ZERO_TERMINATED, 0, &errorcode, &erroroffset, NULL);
+  int string_length;
+  int pattern_length;
+
+  pattern_length = strlen(pattern);
+  re = pcre2_compile((PCRE2_SPTR)pattern, pattern_length, 0, &errorcode, &erroroffset, NULL);
+  
+  if (errorcode != OK_COMPILE_CODE) {
+    print_error(errorcode, erroroffset);
+    return 0;
+  }
 
   // This is just for allocating memory, so it should not be considered part of the timing
   match_data = pcre2_match_data_create_from_pattern(re, NULL);
-  length = strlen(data);
+  string_length = strlen(data);
 
   clock_gettime(CLOCK_MONOTONIC, &start);
-  while (pcre2_match(re, (PCRE2_SPTR8)data, length, offset, 0, match_data, NULL) == 1)
+
+  // Reference this page (https://www.pcre.org/current/doc/html/pcre2demo.html) toward the 
+  // bottom, there is an example of matching multiple times in the same string
+  // This version is prone to errors if something like an empty match occurs. Ideally,
+  // we would fix this, but for now it should work
+  while (pcre2_match(re, (PCRE2_SPTR8)data, string_length, offset, 0, match_data, NULL) > 0)
   {
     count++;
 
@@ -67,6 +96,8 @@ void measure(char *data, char *pattern)
 
   pcre2_match_data_free(match_data);
   pcre2_code_free(re);
+
+  return 1;
 }
 
 int main(int argc, char **argv)
@@ -83,10 +114,12 @@ int main(int argc, char **argv)
 
   for (int i = 0; i < num_iterations; i++)
   {
-    measure(data, pattern);
+    if (!measure(data, pattern)) {
+      return EXIT_FAILURE;
+    }
   }
 
   free(data);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
